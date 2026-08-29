cba112855dd0d2e9070338332706a0b0b9462f37
Stephane MM <stephane.mbesse@gmail.com>
2026-08-29 03:25:16 -0700

    chore(db): index the lead columns used for filtering

---
+++ apps/server/prisma/migrations/20260829102426_index_lead_filters/migration.sql
@@ -0,0 +1,14 @@
+-- CreateIndex
+CREATE INDEX "Lead_createdAt_idx" ON "Lead"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "Lead_sport_idx" ON "Lead"("sport");
+
+-- CreateIndex
+CREATE INDEX "Lead_nationality_idx" ON "Lead"("nationality");
+
+-- CreateIndex
+CREATE INDEX "Lead_gender_idx" ON "Lead"("gender");
+
+-- CreateIndex
+CREATE INDEX "Lead_country_idx" ON "Lead"("country");
